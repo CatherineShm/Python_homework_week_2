9f0a4feb16169d696fb1e9a8f757dd068b9bed8b
CatherineShm <katerynashmaliukh@gmail.com>
2020-05-19 20:12:56 +0200

    corrections were done in zad_3_1 and zad_3_3

--- zad_3_1_funkcje_liczbowe.py
+++ zad_3_1_funkcje_liczbowe.py
@@ -7,17 +7,18 @@ import pytest
 def foots_to_meters(number_of_ft: float or int):
     """
     This function convert foots to meters
-    :param number_of_ft:
-    :return: number of meters, rounded to one sign after point.
+    :param number_of_ft: number of foots that should be converted
+    :return: number of meters
     """
-    return round(number_of_ft / 3.2808, 1)
+    return number_of_ft / 3.2808
+
 
 def max(number_1: int or float, number_2: int or float):
     """
     This function return the bigger of the two given numbers.
-    :param number_1:
-    :param number_2:
-    :return:
+    :param number_1: the first number
+    :param number_2: the second number
+    :return: greater number
     """
     if number_1 < number_2:
         return number_2
@@ -30,9 +31,9 @@ def max(number_1: int or float, number_2: int or float):
 def average(number_1: int or float, number_2: int or float):
     """
     This function calculate the average from two given numbers.
-    :param number_1:
-    :param number_2:
-    :return:
+    :param number_1: the first number
+    :param number_2: the second number
+    :return: the average from two given numbers
     """
     return (number_1 + number_2) / 2
 
@@ -40,10 +41,10 @@ def average(number_1: int or float, number_2: int or float):
 def area_of_circle(radius: int or float):
     """
     This function calculate the area of a circle.
-    :param radius:
-    :return: erea
+    :param radius: provide the radius of a circle
+    :return: area
     """
-    return round(math.pi * (radius ** 2), 0)
+    return math.pi * (radius ** 2)
 
 
 def bmi(height: int or float, weight: int or float):
@@ -51,9 +52,9 @@ def bmi(height: int or float, weight: int or float):
     This function calculate the BMI.
     :param height: the height in meters
     :param weight: the weight in kilograms
-    :return:
+    :return: the BMI
     """
-    return round(weight / (height ** 2), 1)
+    return weight / (height ** 2)
 
 
 def area_of_triangle(a: int or float, b: int or float, c: int or float):
@@ -68,29 +69,32 @@ def area_of_triangle(a: int or float, b: int or float, c: int or float):
         raise SyntaxError("The triangle with those sides doesn't exist.")
     else:
         p = (a + b + c) / 2
-        return round(math.sqrt(p * (p - a) * (p - b) * (p - c)), 1)
+        return math.sqrt(p * (p - a) * (p - b) * (p - c))
+
 
 def km_to_mils(number_of_km: int or float):
-
     """
     This function convert the kilometers on miles, rounded to one sign after point.
-    :param number_of_km:
+    :param number_of_km: kilometers
     :return: miles
     """
-    return round(number_of_km * 0.62137, 1)
+    return number_of_km * 0.62137
+
 
 def miles_to_km(number_of_miles: int or float):
     """
     This function convert the miles to kilometers, rounded to one sign after point.
-    :param number_of_miles:
-    :return:
+    :param number_of_miles: provide miles that should be convented
+    :return: kilometers
     """
-    return round(number_of_miles / 0.62137, 1)
+    return number_of_miles / 0.62137
+
 
 def test_foots_to_meters():
-    assert foots_to_meters(15) == 4.6
-    assert foots_to_meters(0) == 0
-    assert foots_to_meters(64) == 19.5
+    assert foots_to_meters(15) == 4.57205559619605
+    assert foots_to_meters(0) == 0.0
+    assert foots_to_meters(64) == 19.50743721043648
+
 
 def test_max():
      assert max(2, 3) == 3
@@ -98,33 +102,41 @@ def test_max():
      assert max(0, 45) == 45
      assert max(0, 0) == True
 
+
 def test_average():
     assert average(2, 4.2) == 3.1
     assert average(0, -6) == -3
     assert average(0, 5) == 2.5
 
+
 def test_area_of_circle():
-    assert area_of_circle(30) == 2827
-    assert area_of_circle(12) == 452
-    assert area_of_circle(0.1) == 0
-    assert area_of_circle(-6) == 113
+    assert area_of_circle(30) == 2827.4333882308138
+    assert area_of_circle(12) == 452.3893421169302
+    assert area_of_circle(0.1) == 0.031415926535897934
+    assert area_of_circle(-6) == 113.09733552923255
+
 
 def test_bmi():
-    assert bmi(1.6, 50) == 19.5
-    assert bmi(1.8, 90) == 27.8
-    assert bmi(1.2, 80) == 55.6
+    assert bmi(1.6, 50) == 19.531249999999996
+    assert bmi(1.8, 90) == 27.777777777777775
+    assert bmi(1.2, 80) == 55.55555555555556
+
 
 def test_area_of_triangle():
-    assert area_of_triangle(3, 5, 7) == 6.5
-    assert area_of_triangle(10, 16, 14) == 69.3
+    assert area_of_triangle(3, 5, 7) == 6.49519052838329
+    assert area_of_triangle(10, 16, 14) == 69.2820323027551
     with pytest.raises(SyntaxError):
         area_of_triangle(20, 10, 5)
         raise SyntaxError("The triangle with those sides doesn't exist.")
+
+
 def test_km_to_mils():
-    assert km_to_mils(10) == 6.2
-    assert km_to_mils(0) == 0
-    assert km_to_mils(15) == 9.3
+    assert km_to_mils(10) == 6.213699999999999
+    assert km_to_mils(0) == 0.0
+    assert km_to_mils(15) == 9.320549999999999
+
+
 def test_miles_to_km():
-    assert miles_to_km(10) == 16.1
-    assert miles_to_km(0) == 0
-    assert miles_to_km(60) == 96.6
+    assert miles_to_km(10) == 16.093470878864444
+    assert miles_to_km(0) == 0.0
+    assert miles_to_km(60) == 96.56082527318667
--- zad_3_3_operacje_na_listach.py
+++ zad_3_3_operacje_na_listach.py
@@ -1,62 +1,64 @@
 ### Zadanie 3.3 | Operacje na listach
 
-def sum(*args: int):
+
+def sum(numbers=[]):
     """
     Count the sum of the numbers.
-    :param args:
+    :param args: numbers
     :return: sum
     """
     sum = 0
-    for x in args:
+    for x in numbers:
         sum += x
     return sum
 
 
-def average(*args: int):
+def average(numbers=[]):
     """
     Count the average of the numbers.
-    :param args:
-    :return: the average round for 1 sign after point
+    :param args: numbers
+    :return: the average
     """
     sum = 0
     count = 0
-    for number in args:
+    for number in numbers:
         sum += number
         count += 1
-    return round(sum / count, 1)
+    return sum / count
 
 
-def max(*args: int or float):
+def max(numbers=[]):
     """
     Return the greatest number.
     :param args: numbers
-    :return: the greates one
+    :return: the greater one
     """
-    for num in args:
+    for num in numbers:
         number = num
         if num > number:
             number = num
     return number
 
 
-def difference_max_min(*args):
+def difference_max_min(numbers=[]):
     """
     This function count the difference between the max and the min numbers.
     :param args: numbers
-    :return: the difference between max and min
+    :return: the difference between the max and the min
     """
-    if args == ():
+    if numbers == []:
         return 0
-    min = args[0]
-    max = args[0]
-    for number in args:
+    min = numbers[0]
+    max = numbers[0]
+    for number in numbers:
         if min > number:
             min = number
         if max < number:
             max = number
     return max - min
 
-def greater_than(*args, number: int or float):
+
+def greater_than(number: int or float, numbers=[]):
     """
     This return all greater numbers that indicated number.
     :param args: all numbers
@@ -64,25 +66,26 @@ def greater_than(*args, number: int or float):
     :return: all greater numbers
     """
     result = []
-    for character in args:
+    for character in numbers:
         if character > number:
             result.append(character)
     return result
 
 
-def first_reater_than(*args, number: int or float):
+def first_greater_than(number: int or float, numbers=[]):
     """
     This return the first greater number that indicated number.
     :param args: all numbers
     :param number: number
     :return: the first greater number
     """
-    for character in args:
+    for character in numbers:
         if character > number:
             return character
     return None
 
-def sum_of_greater(*args, number: int or float):
+
+def sum_of_greater(number: int or float, numbers=[]):
     """
     This return the sum of all greater numbers that indicated number.
     :param args: all numbers
@@ -90,7 +93,7 @@ def sum_of_greater(*args, number: int or float):
     :return: all greater numbers
     """
     all_greater = []
-    for character in args:
+    for character in numbers:
         if character > number:
             all_greater.append(character)
     result = 0
@@ -98,7 +101,8 @@ def sum_of_greater(*args, number: int or float):
         result += numbers
     return result
 
-def count_of_greater(*args, number: int or float):
+
+def count_of_greater(number: int or float, numbers=[]):
     """
     This function count all greater numbers that indicated number.
     :param args: all numbers
@@ -106,7 +110,7 @@ def count_of_greater(*args, number: int or float):
     :return: count of greater numbers
     """
     count = 0
-    for character in args:
+    for character in numbers:
         if character > number:
             count += 1
     if count == 0:
@@ -114,7 +118,8 @@ def count_of_greater(*args, number: int or float):
     else:
         return count
 
-def divisible(*args, number: int or float):
+
+def divisible(number: int or float, numbers=[]):
     """
     This function return all numbers that are divisible by the indicated number
     :param args: all numbers
@@ -125,7 +130,7 @@ def divisible(*args, number: int or float):
         return "Chcesz dzielić przez zero? Zastanów się."
 
     all_divisible = []
-    for character in args:
+    for character in numbers:
         if character % number == 0:
             all_divisible.append(character)
     if all_divisible == []:
@@ -133,7 +138,8 @@ def divisible(*args, number: int or float):
     else:
         return all_divisible
 
-def first_divisible(*args, number: int or float):
+
+def first_divisible(number: int or float, numbers=[]):
     """
     This function return the first number that is divisible by the indicated number
     :param args: all numbers
@@ -142,7 +148,7 @@ def first_divisible(*args, number: int or float):
     """
     if number == 0:
         return "Chcesz dzielić przez zero? Zastanów się."
-    for character in args:
+    for character in numbers:
         if character % number == 0:
             return character
     return None
@@ -161,42 +167,62 @@ def find_joint(numbers1: set, numbers2: set):
         return result
 
 def test_sum():
-    assert sum(5, 10, 1) == 16
-    assert sum(-2, 0, 51) == 49
+    assert sum([5, 10, 1]) == 16
+    assert sum([-2, 0, 51]) == 49
+
+
 def test_average():
-    assert average(5, 13) == 9
-    assert average(10, 0, -2) == 2.7
+    assert average([5, 13]) == 9
+    assert average([11, 0, -2]) == 3
+
+
 def test_max():
-    assert max(4, 2, 49) == 49
-    assert max(-50, 0, 60) == 60
+    assert max([4, 2, 49]) == 49
+    assert max([-50, 0, 60]) == 60
+
+
 def test_difference_max_min():
-    assert difference_max_min(2, 3, 5) == 3
-    assert difference_max_min(-4, 10, 4) == 14
-    assert difference_max_min() == 0
+    assert difference_max_min([2, 3, 5]) == 3
+    assert difference_max_min([-4, 10, 4]) == 14
+    assert difference_max_min([]) == 0
+
+
 def test_greater_than():
-    assert greater_than(2, -5, 0, 4, number=0) == [2, 4]
-    assert greater_than(-10, 13, 10, 2, number=5) == [13, 10]
+    assert greater_than(number=0, numbers=[2, -5, 0, 4]) == [2, 4]
+    assert greater_than(number=5, numbers=[-10, 13, 10, 2]) == [13, 10]
+
+
 def test_first_reater_than():
-    assert first_reater_than(-10, 13, 10, 2, number=5) == 13
-    assert first_reater_than(-2, 0, 4, 6, number=2) == 4
-    assert first_reater_than(0, 2, 5, number= 7) == None
+    assert first_greater_than(numbers=[-10, 13, 10, 2], number=5) == 13
+    assert first_greater_than(numbers=[-2, 0, 4, 6], number=2) == 4
+    assert first_greater_than(numbers=[0, 2, 5], number= 7) == None
+
+
 def test_sum_of_greater():
-    assert sum_of_greater(2, -5, 0, 4, number=0) == 6
-    assert sum_of_greater(-10, 13, 10, 2, number=5) == 23
+    assert sum_of_greater(numbers=[2, -5, 0, 4], number=0) == 6
+    assert sum_of_greater(numbers=[-10, 13, 10, 2], number=5) == 23
+
+
 def test_count_of_greater():
-    assert count_of_greater(2, -5, 0, 4, number=0) == 2
-    assert count_of_greater(-10, 13, 2, number=5) == 1
-    assert count_of_greater(10, 0, 2, number=12) == None
+    assert count_of_greater(numbers=[2, -5, 0, 4], number=0) == 2
+    assert count_of_greater(numbers=[-10, 13, 2], number=5) == 1
+    assert count_of_greater(numbers=[10, 0, 2], number=12) == None
+
+
 def test_divisible():
-    assert divisible(1, 3, 4, 6, number=2) == [4, 6]
-    assert divisible(-3, 12, 14, number=3) == [-3, 12]
-    assert divisible(0, 1, 2, number=0) == "Chcesz dzielić przez zero? Zastanów się."
-    assert divisible(2, 3, 7, 9, number=5) == None
+    assert divisible(numbers=[1, 3, 4, 6], number=2) == [4, 6]
+    assert divisible(numbers=[-3, 12, 14], number=3) == [-3, 12]
+    assert divisible(numbers=[0, 1, 2], number=0) == "Chcesz dzielić przez zero? Zastanów się."
+    assert divisible(numbers=[2, 3, 7, 9], number=5) == None
+
+
 def test_first_divisible():
-    assert first_divisible(1, 3, number=1) == 1
-    assert first_divisible(-5, 4, 6, 9, number=3) == 6
-    assert first_divisible(1, 2, 3, number=0) == "Chcesz dzielić przez zero? Zastanów się."
-    assert first_divisible(2, 3, 6, 9, number=5) == None
+    assert first_divisible(numbers=[1, 3], number=1) == 1
+    assert first_divisible(numbers=[-5, 4, 6, 9], number=3) == 6
+    assert first_divisible(numbers=[1, 2, 3], number=0) == "Chcesz dzielić przez zero? Zastanów się."
+    assert first_divisible(numbers=[2, 3, 6, 9], number=5) == None
+
+
 def test_find_joint():
     assert find_joint({1, 2 , 3}, {2, 3, 4}) == {2, 3}
     assert find_joint({2, 5, -10}, {0, -10, 4}) == {-10}
